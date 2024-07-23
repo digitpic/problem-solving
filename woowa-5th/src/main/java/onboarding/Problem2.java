adb93faa549e64008f4add0a0a4a95655b9d70ea
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-07-23 14:48:10 +0900

    feat: Add removeDuplicatedChar method

--- woowa-5th/src/main/java/onboarding/Problem2.java
+++ woowa-5th/src/main/java/onboarding/Problem2.java
@@ -23,4 +23,21 @@ public class Problem2 {
         return crypto;
     }
 
+    public static String removeDuplicatedChar(String crypto){
+
+        // 중복된 문자를 찾아서 제거
+        for (int i = 0; i < crypto.length() - 1; i++){
+
+            // 현재 문자와 다음 문자가 같은 경우
+            if (crypto.charAt(i) == crypto.charAt(i + 1)){
+
+                // 중복된 문자 제거
+                crypto = crypto.substring(0, i) + crypto.substring(i + 2);
+            }
+        }
+
+        // 중복된 문자 제거한 결과 반환
+        return crypto;
+    }
+
 }
