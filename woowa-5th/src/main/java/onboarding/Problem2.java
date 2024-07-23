b9a12a3a438e3408cd39df047533fc2705f01568
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-07-23 14:46:55 +0900

    feat: Modify solution method

--- woowa-5th/src/main/java/onboarding/Problem2.java
+++ woowa-5th/src/main/java/onboarding/Problem2.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+// 2번 문제 클래스
 public class Problem2 {
-    public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+    // 문제 풀이 메인 메서드
+    public static String solution(String crypto){
+
+        // 암호문에서 중복된 문자 찾아서 계속 제거하기
+        while (true){
+
+            // 중복된 문자를 찾아서 제거
+            String removed = removeDuplicatedChar(crypto);
+
+            // 중복된 문자가 더 이상 없는 경우
+            if (removed.equals(crypto)){
+                break; // 반복문 종료
+            }
+
+            // 중복된 문자가 있는 경우
+            crypto = removed; // 암호문 갱신
+        }
+        return crypto;
     }
+
 }
