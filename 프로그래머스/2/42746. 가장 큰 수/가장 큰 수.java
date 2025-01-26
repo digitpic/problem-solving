3073c95122364c63e78ba5a2a813302cf7f058d7
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-01-26 10:21:31 +0900

    [level 2] Title: 가장 큰 수, Time: 177.58 ms, Memory: 151 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+/*
+주어진 숫자들을 조합해서 만들 수 있는 가장 큰 수 리턴
+*/
+
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(final int[] numbers) {
+    
+        List<String> stringNumbers = Arrays.stream(numbers)
+                                           .mapToObj(Integer::toString)
+                                           .collect(Collectors.toList());
+
+        stringNumbers.sort(
+            (number1, number2) -> (number2 + number1).compareTo(number1 + number2)
+        );
+
+
+        StringBuilder result = new StringBuilder();
+        for (final String number : stringNumbers) {
+            result.append(number);
+        }
+        
+        if (stringNumbers.get(0).equals("0")) {
+            return "0";
+        }
+
+        return result.toString();
+    }
+}
