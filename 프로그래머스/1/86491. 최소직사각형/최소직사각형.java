4e1f631cdcc954a877cca6ac752a7d66b29ca80e
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-01-30 20:57:53 +0900

    [level 1] Title: 최소직사각형, Time: 1.99 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -6,11 +6,24 @@ class Solution {
         int maxWidth = Integer.MIN_VALUE;
         int maxHeight = Integer.MIN_VALUE;
         
+        // 전체 순회
         for (final int[] card : sizes) {
-            maxWidth = Math.max(maxWidth, Math.max(card[WIDTH], card[HEIGHT]));
-            maxHeight = Math.max(maxHeight, Math.min(card[WIDTH], card[HEIGHT]));
+            // 명함의 가로, 세로 중 더 `긴` 값 찾기
+            final int maxLengthInCard = Math.max(card[WIDTH], card[HEIGHT]);
+            
+            // 가로, 세로 중 더 긴 값을 가로 최댓값으로 지정
+            // 명함 전부 눕혀버리기
+            maxWidth = Math.max(maxWidth, maxLengthInCard);
+            
+            // 명함의 가로, 세로 중 더 `짧은` 값 찾기
+            final int minLengthInCard = Math.min(card[WIDTH], card[HEIGHT]);
+            
+            // 가로, 세로 중 더 짧은 값을 세로 최댓값으로 지정?
+            // 명함 전부 눕혀버리기
+            maxHeight = Math.max(maxHeight, minLengthInCard);
         }
         
+        // 최소 지갑 크기 리턴
         return maxWidth * maxHeight;
     }
 }
