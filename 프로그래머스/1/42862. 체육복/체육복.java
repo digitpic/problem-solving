6d7afdafc4f14f63e885bf9e49442f74bfeda68e
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-02-05 09:43:34 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,7 +2,8 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int[] people = new int[n];
         int answer = n;
-
+        
+        // 초기 세팅
         for (int l : lost) {
             int lostIndex = l - 1;
             people[lostIndex]--;
@@ -14,14 +15,23 @@ class Solution {
         }
 
         for (int i = 0; i < people.length; i++) {
+            // i 번 학생이 체육복을 도난 당했다면
             if (people[i] == -1) {
+                
+                // i - 1 번 학생이 여분이 있다면
                 if (i - 1 >= 0 && people[i - 1] == 1) {
                     people[i]++;
                     people[i - 1]--;
-                } else if (i + 1 < people.length && people[i + 1] == 1) {
+                } 
+                
+                // i + 1 번 학생이 여분이 있다면
+                else if (i + 1 < people.length && people[i + 1] == 1) {
                     people[i]++;
                     people[i + 1]--;
-                } else {
+                }
+                
+                // i - 1, i + 1 번 학생 둘 다 여분이 없다면
+                else {
                     answer--;
                 }
             }
