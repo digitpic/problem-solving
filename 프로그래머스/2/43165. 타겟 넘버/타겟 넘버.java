4f5a64899ec090e5d58048c5f68e11fa8cf5eeb5
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-02-06 09:28:28 +0900

    [level 2] Title: 타겟 넘버, Time: 7.56 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,35 @@
+/*
+n 개의 음이 아닌 정수
+순서를 바꾸지 않고, 적절히 더하거나 빼서 타겟 넘버 만들기
+
+numbers: 사용할 수 있는 숫자가 담긴 배열
+target: 타겟 넘버
+return: 타겟 넘버를 만드는 방법의 수
+*/
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        
+        answer = dfs(numbers, target, 0, 0);
+        
+        return answer;
+    }
+    
+    public int dfs(int[] numbers, int target, int sum, int count) {
+        // 전부 돌았다면
+        if (count == numbers.length) {
+            
+            // 타겟 넘버가 만들어지면
+            if (sum == target) {
+                return 1;
+            }
+            // 만들어지지 않으면
+            return 0;
+        }
+        
+        // 전부 돌지 않았다면
+        return dfs(numbers, target, sum + numbers[count], count + 1) 
+            + dfs(numbers, target, sum - numbers[count], count + 1);
+    }
+}
