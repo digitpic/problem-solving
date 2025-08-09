e0449bd8d3e87b4ab99aeae32a39762e30dd55f6
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-08-09 09:41:40 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?itm_content=course14743) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 0.03 ms
+메모리: 90.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 05일 10:07:58
+2025년 08월 09일 09:41:37
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,37 +1,29 @@
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int[] people = new int[n];
-        int answer = n;
+    private static final int MAX = 30;
+    
+    private static int[] people = new int[MAX];
+    private static int answer;
         
-        // 초기 세팅
+    public int solution(int n, int[] lost, int[] reserve) {
+        answer = n;
+        
         for (int l : lost) {
-            int lostIndex = l - 1;
-            people[lostIndex]--;
+            people[l - 1]--;
         }
             
         for (int r : reserve) {
-            int reserveIndex = r - 1;
-            people[reserveIndex]++;
+            people[r - 1]++;
         }
 
         for (int i = 0; i < people.length; i++) {
-            // i 번 학생이 체육복을 도난 당했다면
             if (people[i] == -1) {
-                
-                // i - 1 번 학생이 여분이 있다면
-                if (i > 0 && people[i - 1] == 1) {
+                if (0 < i && people[i - 1] == 1) {
                     people[i]++;
                     people[i - 1]--;
-                } 
-                
-                // i + 1 번 학생이 여분이 있다면
-                else if (i + 1 < n && people[i + 1] == 1) {
+                } else if (i + 1 < n && people[i + 1] == 1) {
                     people[i]++;
                     people[i + 1]--;
-                }
-                
-                // i - 1, i + 1 번 학생 둘 다 여분이 없다면
-                else {
+                } else {
                     answer--;
                 }
             }
