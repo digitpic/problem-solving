cb09c04a90218108dd83c7a507b3cf3c8e80633c
Kyuwon Jeong <63178849+digitpic@users.noreply.github.com>
2025-08-09 18:36:25 +0900

    [level 3] Title: 여행경로, Time: 0.55 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+
+    private final List<String> answer = new ArrayList<>();
+    private final Map<String, PriorityQueue<String>> graph = new HashMap<>();
+
+    public String[] solution(String[][] tickets) {
+        for (String[] ticket : tickets) {
+            String from = ticket[0];
+            String to = ticket[1];
+
+            graph.putIfAbsent(from, new PriorityQueue<>());
+            graph.get(from).add(to);
+        }
+
+        dfs("ICN");
+
+        Collections.reverse(answer);
+
+        return answer.toArray(new String[0]);
+    }
+
+    private void dfs(String current) {
+        while (graph.containsKey(current) && !graph.get(current).isEmpty()) {
+            String next = graph.get(current).poll();
+            dfs(next);
+        }
+        
+        answer.add(current);
+    }
+    
+}
